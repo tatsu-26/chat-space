965d813060ec452a732bad532cbeb1cba1f7db9e
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-09-09 17:14:45 +0900

    グループ機能実装１

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index", except: :index
+  root "messages#index"
   resources :users, only: [:edit, :update]
-
+  resources :groups, only: [:new, :create, :update, :edit] 
+    
 end
