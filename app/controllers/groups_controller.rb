1f9f2e0e915498a4a0a4fbb278b89aa84cee0d76
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-09-10 14:40:14 +0900

    メッセージ送信機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
   def index
 
   end
@@ -24,12 +24,21 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to groups_path(@group), notice: "グループを編集しました"
+    else
+      render :edit
+    end
   end
 
   
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
   
     
