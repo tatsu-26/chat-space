90c52f95304f54a96bdd29d3b4f8f2d59fd0f0b1
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-09-22 18:16:44 +0900

    修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: "グループを作成しました"
+      
     else
       render :new
     end
@@ -25,7 +26,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to groups_path(@group), notice: "グループを編集しました"
+      redirect_to group_messages_path(@group), notice: "グループを編集しました"
     else
       render :edit
     end
